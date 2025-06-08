4b70e6472cf777fb5e21ed3d96645d493bfef4e1
りんご飴196 <kazumac41@gmail.com>
2025-06-08 20:56:46 +0900

    ✨ 爆発ブロック設定

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -1,5 +1,8 @@
 package com.github.ringoame196_s_mcPlugin
 
+import org.bukkit.Material
+
 object Data {
     const val RANDOM_ORE_WORLD_NAME = "random_ore"
+    val explosionBlock = Material.TNT
 }
